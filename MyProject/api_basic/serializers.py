f284fac6d8d5de7c7d00590d67ebca6103bbc7c0
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-10-23 00:10:02 +0530

    function based API Views

--- MyProject/api_basic/serializers.py
+++ MyProject/api_basic/serializers.py
@@ -5,7 +5,7 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        field = ['id', 'title', 'author']
+        fields = ['id', 'title', 'author']
 
 
 # class ArticleSerializer(serializers.Serializer):
