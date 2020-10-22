ac6a35ef1f56ee5086cf30d51028aaacf77701e7
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-10-22 23:15:32 +0530

    practice serialization

--- MyProject/api_basic/models.py
+++ MyProject/api_basic/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 
-class Articles(models.Model):
+class Article(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
