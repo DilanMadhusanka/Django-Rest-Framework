2fb570c08366db33942d5f72c8a3c4ce5404ca9f
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-10-25 22:51:09 +0530

    create class based api view for get post

--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -1,15 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from django.http import HttpResponse
 from .models import Article
 from .serializers import ArticleSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 
 # Create your views here.
+
+class ArticleAPIView(APIView):
+
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
 
 @api_view(['GET', 'POST'])
 def article_list(request):
