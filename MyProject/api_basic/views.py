77451c2d5447e4ec1b321e61722375fcbee2fc0b
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-10-25 23:24:33 +0530

    create class api view for put delete operations

--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from .models import Article
 from .serializers import ArticleSerializer
 from rest_framework.decorators import api_view
@@ -25,8 +24,33 @@ class ArticleAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class ArticleDetails(APIView):
 
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
 
+        except Article.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        article = self.get_object(id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET', 'POST'])
@@ -51,7 +75,7 @@ def article_detail(request, pk):
         article = Article.objects.get(pk=pk)
 
     except Article.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
